a4c39a2caa6f4187f9ca5252ef1658822a23d3dd
Deepanshu-goswami <Deepugoswami1995@gmail.com>
2023-01-23 15:33:54 +0000

    remove

--- app_engine_files/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var handleRequest = function(request, response) {
-  response.writeHead(200);
-  response.end("<h1>Deepanshu Goswami Welcomes You Using Google App Engine Standard version2</h1>");
-}
-var www = http.createServer(handleRequest);
-www.listen(8080);
